ee1c16a919cbe63e70b7a2a63e24f7b83d41b8df
Xavier O'Rourke <xavier.orourke@gmail.com>
2020-10-22 09:35:49 +1000

    Skeleton of the backend

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import todoRoutes from "./routes";
+import bodyParser from "body-parser";
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(todoRoutes);
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@owlpostdev.vel0i.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.set("useFindAndModify", false);
+
+app.get("/ping", (req, res) => {
+  res.send("yoyoyo");
+});
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => {
+    throw error;
+  });
