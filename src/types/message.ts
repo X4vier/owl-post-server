1bd59557d7f8c1bbed6677570feb75b4ed690759
Xavier O'Rourke <xavier.orourke@gmail.com>
2020-11-03 04:15:40 +1000

    Added IMessage type

---
+++ src/types/message.ts
@@ -0,0 +1,11 @@
+import { Document } from "mongoose";
+
+interface IMessage extends Document {
+  from: string;
+  to: string;
+  status: "unsent" | "in-flight" | "delivered" | "opened";
+  sentDate: Date | null;
+  arrivalDate: Date | null;
+}
+
+export { IMessage };
