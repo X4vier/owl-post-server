ce408efbfec105aa03454f26746f68b58c8f94ff
Xavier O'Rourke <xavier.orourke@gmail.com>
2020-11-03 04:23:04 +1000

    Deleted createUser function

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,6 +1,5 @@
 export * from "./emailExists";
 export * from "./userNameExists";
-export * from "./createUser";
 export * from "./getUserByEmailOrName";
 export * from "./getUsers";
 export * from "./signUp";
